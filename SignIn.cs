d1ff4ce877ecd90e1ab3754c188cb8c600669b94
priyanka <priyankatripathy81@gmail.com>
2024-02-24 12:17:59 +0530

    add files

---
+++ SignIn.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WelcomeApp
+{
+   public  class SignIn
+    {
+        public string Authenticate(string userName, string password)
+        {
+            string msg;
+            if ((string.IsNullOrEmpty(userName)) || string.IsNullOrEmpty(password))
+            {
+                msg = "User name and password Required";
+            }
+            else
+            {
+                if((userName == "admin") && (password == "admin@1256"))
+                {
+                    msg = "Authentication Pass";
+                }
+                else
+                {
+                    msg = "Authentication Fail";
+                }
+            }
+                return msg;
+            
+        }
+    }
+}
